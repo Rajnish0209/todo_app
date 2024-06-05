c5fdb5ecae8974ab12bc150712c9d7e62b5109fc
Rajnish0209 <rajnishkumargupta331@gmail.com>
2024-06-05 10:16:36 +0530

    add todo html,css,javascript file with basic html,css,javascript code

---
+++ script.js
@@ -0,0 +1,34 @@
+let inp = document.querySelector("input");
+
+let btn = document.querySelector("button");
+
+let unorderdList = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+  let listItem = document.createElement("li");
+  listItem.innerText = inp.value;
+
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "Delete";
+  delBtn.classList.add("delete");
+  listItem.appendChild(delBtn);
+
+  unorderdList.appendChild(listItem);
+  inp.value = "";
+});
+
+// let delBtns = document.querySelectorAll(".delete");
+// for (delBtn of delBtns) {
+//   delBtn.addEventListener("click", function () {
+//     let parent = this.parentElement;
+//     parent.remove();
+//   });
+// }
+
+unorderdList.addEventListener("click", function (event) {
+  if (event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement;
+    listItem.remove();
+  }
+});
+
